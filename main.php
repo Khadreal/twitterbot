7aba3eacead18cae10077cf6e6deffb987e5acf0
Opeyemi Ibrahim <sirkhadrealryan@gmail.com>
2018-03-08 14:57:46 +0100

    Storing search in google spreadsheet

--- main.php
+++ main.php
@@ -1,91 +1,71 @@
 <?php
 
 require "vendor/autoload.php";
+//require "doc.php";
 
 use Abraham\TwitterOAuth\TwitterOAuth;
-
-
-class Main
-{
-
-	private $sheet_id = "Twitterbot";
-	private $application_name = "Twitter Bot";
-	private $client_secret_path = __DIR__ . '/sheets_api_secret.json' ;
-	private $access_token = "";
-	
+use Google\Spreadsheet\DefaultServiceRequest;
+use Google\Spreadsheet\ServiceRequestFactory;
 
 
 
-	private $conn;
-	private $client;
-	private $aSearchFilters = array();
-	private $aDiceValues = array();
-	private $consumerKey = "5DRHOI34z4Eb2P5INeZnTn5dP";
-	private $consumerSecret = "uH10YyynLJZIbMEQk4Zq42UHSJBmXvbhT98Kmr8yRaHBJON6AU";
-	private $accessToken = "269931982-lvZn8v0Uf8hW9UKl2SqRFQesvWeqrb6knreHSgzw";
-	private $accessTokenSecret = "1UGNYBESwuTHxeLgCVU8QAqdWxLpYLLqRHO2EQZuIAPUK" ;
+ 	$aDiceValues = array();
+	$consumerKey = "5DRHOI34z4Eb2P5INeZnTn5dP";
+	$consumerSecret = "uH10YyynLJZIbMEQk4Zq42UHSJBmXvbhT98Kmr8yRaHBJON6AU";
+	$accessToken = "269931982-lvZn8v0Uf8hW9UKl2SqRFQesvWeqrb6knreHSgzw";
+	$accessTokenSecret = "1UGNYBESwuTHxeLgCVU8QAqdWxLpYLLqRHO2EQZuIAPUK" ;
 
 
-	public function __construct
-	(
+	$connection =  new TwitterOAuth($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
+
+	putenv('GOOGLE_APPLICATION_CREDENTIALS=' . __DIR__ . '/client_secret.json');
+	$client = new Google_Client;
+	$client->useApplicationDefaultCredentials();
+	 
+	$client->setApplicationName("Something to do with my representatives");
+	$client->setScopes(['https://www.googleapis.com/auth/drive','https://spreadsheets.google.com/feeds']);
+	 
+	if ($client->isAccessTokenExpired()) {
+	    $client->refreshTokenWithAssertion();
+	}
+	 
+	$accessToken = $client->fetchAccessTokenWithAssertion()["access_token"];
+	ServiceRequestFactory::setInstance(
+	    new DefaultServiceRequest($accessToken)
+	);
+
+
+	$spreadsheetService = new Google\Spreadsheet\SpreadsheetService();
+	$spreadsheetFeed = $spreadsheetService->getSpreadsheetFeed();
+
+
+	$spreadsheet = $spreadsheetFeed->getByTitle('Twitter Bot');
+
+	$worksheets = $spreadsheet->getWorksheetFeed()->getEntries();
+
+	$worksheet = $worksheets[1];
+	$listFeed = $worksheet->getListFeed();
+	$cellFeed = $worksheet->getCellFeed();
+	$cellFeed->editCell(1,1, "email");
+	$cellFeed->editCell(1,2, "age");
+
+	// Search for users with tweet that contains a certain hashtag
+	$data = $connection->get("users/search", ["q" => "Twitter", "page" => 1, "count" => 20]);
+
+
+	foreach ($data as $user)
+	{
+
+		echo $user->name ."  -   " ;
+		echo $user->followers_count ."<br>" ;
 		
-	)
-	{
-		$this->scopes = implode(' ', [Google_Service_Sheets::SPREADSHEETS]);
 
-		$this->conn		=	new TwitterOAuth($this->consumerKey, $this->consumerSecret, $this->accessToken, $this->accessTokenSecret);
+		$listFeed->insert([
+	        'email' => $user->name,
+	        'age'	=> $user->followers_count
+	    ]);
 
-		$client = new Google_Client();
-		$client->setApplicationName($this->application_name);
-		$client->setScopes($this->scopes);
-		$client->setAuthConfig($this->client_secret_path);
-		$client->setAccessToken($this->access_token);
-	}
 
-	public function run()
-	{
-		$data = $this->conn->get("users/search", ["q" => "Twitter", "page" => 1, "count" => 20]);
-		
-		//var_dump($data);
-		return $data;
-		return json_decode(json_encode($data),true);
-	}
-
-	public function spreadsheets()
-	{
-		$spreadsheet = (new Google\Spreadsheet\SpreadsheetService)
-		   ->getSpreadsheetFeed()
-		   ->getByTitle('Copy of Legislators 2017');
-		 
-		// Get the first worksheet (tab)
-		$worksheets = $spreadsheet->getWorksheetFeed()->getEntries();
-		$worksheet = $worksheets[0];
-		return  $worksheet;
-	}
-
-	public function worksheet()
-	{
-		$listFeed = $this->spreadsheets->getListFeed();
-
-		
-		foreach ($listFeed->getEntries() as $entry) {
-		  echo $representative = $entry->getValues();
-		}
 	}
 
 
-}
-
-
-$scripts = new Main;
-
-$users = $scripts->run();
-
-$scripts->worksheet();
-
-/*foreach ($users as $user)
-{
-	echo $user->name ."  -   " ;
-	echo $user->followers_count ."<br>" ;
-
-}*/
